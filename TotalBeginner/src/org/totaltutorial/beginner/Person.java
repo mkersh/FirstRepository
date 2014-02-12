45db74fc80ddac139d306319764ec6577e374fa0
mkersh <kershaw.mark@gmail.com>
2014-02-12 15:06:04 +0000

    First checkin

---
+++ TotalBeginner/src/org/totaltutorial/beginner/Person.java
@@ -0,0 +1,30 @@
+package org.totaltutorial.beginner;
+
+public class Person {
+	//fields
+	private String name; // name of the person
+	private int maximumBooks; // most books person can check out
+	
+	//constructors
+	public Person() {
+		name = "unknown name";
+		maximumBooks = 3;
+	}
+	
+	// methods 
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String anyName) {
+		name = anyName;
+	}
+
+	public int getMaximumBooks() {
+		return maximumBooks;
+	}
+
+	public void setMaximumBooks(int maximumBooks) {
+		this.maximumBooks = maximumBooks;
+	}
+}
