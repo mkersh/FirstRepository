1dad561f386ebbf8dc5d3fde4325aed34356e306
mkersh <kershaw.mark@gmail.com>
2014-02-14 18:19:28 +0000

    First cut not balanced

--- BinaryTree/src/org/mk/java/tests/BinaryTree/BinaryTree.java
+++ BinaryTree/src/org/mk/java/tests/BinaryTree/BinaryTree.java
@@ -20,7 +20,7 @@ public class BinaryTree {
 	
 	// Methods
 	
-	public void add(String itemToAdd){
+	public void add(String itemToAdd) throws Exception{
 		count += 1;
 		rootNode.add(itemToAdd);
 	}
@@ -35,7 +35,7 @@ public class BinaryTree {
 	}
 	
 	public String toString(){
-		return null;
+		return rootNode.getTreeAsString(1);
 	}
 	
 
@@ -49,16 +49,26 @@ class BinaryTreeNode {
 	//Constructors
 	public BinaryTreeNode(){}
 	
+	// methods
+	
+	private int compareData(String data ){
+		int i1 = Integer.parseInt(this.data);
+		int i2 = Integer.parseInt(data);
+		if (i1 == i2) return 0;
+		else if (i1 < i2) return -1;
+		else return 1;
+	}
+	
 	public String find(String itemToFind) {
 		if (this.data == null) {
 			// Must be an empty Tree return null
 			return null;
 		}
-		else if (this.data.compareTo(itemToFind) == 0){
+		else if (this.compareData(itemToFind) == 0){
 			// We have a match
 			return this.data;
 		}
-		else if (this.data.compareTo(itemToFind) < 0){
+		else if (this.compareData(itemToFind) > 0){
 			if (this.left == null) return null;
 			return this.left.find(itemToFind);
 		}
@@ -68,14 +78,17 @@ class BinaryTreeNode {
 		}
 	}
 
-	// methods
-	public void add(String itemToAdd){
+	public void add(String itemToAdd) throws Exception{
 		if (this.data == null) {
 			// empty tree, store in this node
 			this.data = itemToAdd;
 			
 		}
-		else if (this.data.compareTo(itemToAdd) <= 0){
+		else if (this.compareData(itemToAdd) == 0){
+			// Throw an Exception
+			throw new Exception("Duplicate Item");
+		}
+		else if (this.compareData(itemToAdd) > 0){
 			if (this.left == null) this.left = new BinaryTreeNode();
 			this.left.add(itemToAdd);
 		}
@@ -84,5 +97,30 @@ class BinaryTreeNode {
 			this.right.add(itemToAdd);
 		}
 		
+	}
+	
+	public String getTreeAsString(int depth){
+		String str = "";
+		if (this.left != null){
+			str += this.left.getTreeAsString(depth+1);
+			System.out.println("After left: " + str);
+		}
+		if (this.data != null){
+			str += padString("", depth, '-');
+			str += this.data + "\n";
+			System.out.println("Node: " + str);
+		}
+		if (this.right != null){
+			str += this.right.getTreeAsString(depth+1);
+			System.out.println("After right: " + str);
+		}
+		
+		return str;
+	}
+	
+	private static String padString(String str, int leng,char chr) {
+        for (int i = str.length(); i < leng; i++)
+            str += chr;
+        return str;
 	}
 }
--- BinaryTree/test/org/mk/java/tests/BinaryTree/testBinaryTree.java
+++ BinaryTree/test/org/mk/java/tests/BinaryTree/testBinaryTree.java
@@ -10,8 +10,12 @@ public class testBinaryTree {
 	private BinaryTree bt;
 
 	@Before
-	public void setup(){
+	public void setup() throws Exception{
 		bt = new BinaryTree();
+		bt.add("10");
+		bt.add("20");
+		bt.add("30");
+		bt.add("40");
 		bt.add("1");
 		bt.add("2");
 		bt.add("3");
@@ -20,17 +24,27 @@ public class testBinaryTree {
 
 	@Test
 	public void testAdd() {
-		assertEquals(4,bt.count);
+		assertEquals(8,bt.count);
 	}
 	
 	@Test
 	public void testFind() {
-		assertEquals("2", bt.find("2"));
-		assertEquals(null, bt.find("7"));
+		assertEquals("20", bt.find("20"));
+		assertEquals(null, bt.find("70"));
 	}
 
 	@Test
 	public void testToString() {
-		fail("Not yet implemented");
+		String btStr = "";
+		btStr += "--1\n";
+		btStr += "---2\n";
+		btStr += "----3\n";
+		btStr += "-----4\n";
+		btStr += "-10\n";
+		btStr += "--20\n";
+		btStr += "---30\n";
+		btStr += "----40\n";
+		
+		assertEquals(btStr , bt.toString());
 	}
 }
