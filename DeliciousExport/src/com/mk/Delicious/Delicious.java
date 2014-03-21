fe6a266b2c4618fb3c3691f05cb12ca008705def
mkersh <kershaw.mark@gmail.com>
2014-03-21 15:46:35 +0000

    Tidied it up so that we inject dependencies into the Main class

--- DeliciousExport/src/com/mk/Delicious/Delicious.java
+++ DeliciousExport/src/com/mk/Delicious/Delicious.java
@@ -1,31 +1,18 @@
 package com.mk.Delicious;
 import javax.xml.parsers.*;
-import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathConstants;
-import javax.xml.xpath.XPathExpressionException;
-import javax.xml.xpath.XPathFactory;
-
-import org.w3c.dom.Document;
-import org.w3c.dom.NodeList;
-import org.w3c.dom.Node;
-import org.w3c.dom.Element;
 import org.xml.sax.SAXException;
-
-import java.io.File;
 import java.io.IOException;
-import java.util.Hashtable;
 
 import org.springframework.context.*;
 import org.springframework.context.support.*;
 
 public class Delicious {
 	
-	private ApplicationContext ctx = null;
+	// Using Spring to inject into the following properties
 	private ParseDelicious parser;
 	private DeliciousVisitor visitor;
 	
 	public Delicious(){
-		ctx = new FileSystemXmlApplicationContext("file:/home/mark/git/FirstRepository/DeliciousExport/delicious-beans.xml");
 	}
 
 	/***
@@ -37,16 +24,33 @@ public class Delicious {
 	 * @throws IOException 
 	 */
 	public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException {	
-		Delicious del = new Delicious();
+		ApplicationContext ctx = new FileSystemXmlApplicationContext("file:/home/mark/git/FirstRepository/DeliciousExport/delicious-beans.xml");
+		
+		Delicious del = (Delicious)ctx.getBean("Delicious");
 		del.parseDeliciousXML();
+		((FileSystemXmlApplicationContext) ctx).close();
 	}
 	
+	public ParseDelicious getParser() {
+		return parser;
+	}
+
+	public void setParser(ParseDelicious parser) {
+		this.parser = parser;
+	}
+
+	public DeliciousVisitor getVisitor() {
+		return visitor;
+	}
+
+	public void setVisitor(DeliciousVisitor visitor) {
+		this.visitor = visitor;
+	}
+
 	/***
 	 * Going introduce Spring to DI implementation instances of ParseDelicious and DeliciousVisitor to use
 	 */
 	public void parseDeliciousXML(){
-		parser = (ParseDelicious) ctx.getBean("ParseDelicious");
-		visitor = (DeliciousVisitor) ctx.getBean("DeliciousVisitor");
 		DeliciousPropertyFile prop = DeliciousPropertyFile.getInstance();
 		
 		parser.parse(prop.getProperty("DeliciousFile"),visitor);
